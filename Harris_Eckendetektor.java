b64647bff2c60c84a726500a7904c8c2bd32cbbc
Lorenz Gillner <mail@lorenzgillner.xyz>
2021-04-24 22:58:55 +0200

    Harrisdetektor - jetzt mit Gradientenbildern!

--- Harris_Eckendetektor.java
+++ Harris_Eckendetektor.java
@@ -7,27 +7,10 @@ import ij.util.ArrayUtil;
 import java.awt.*;
 import java.lang.Math;
 import java.util.ArrayList;
-// import java.util.Collections;
+import java.util.Collections;
 
 public class Harris_Eckendetektor implements PlugInFilter {
 	
-	/* Punkt-Klasse zur Speicherung der Eckpunkte */
-	public class Point { 
-		private int x;
-		private int y;
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-		/* eventuell ohne this */
-		public int getX() {
-			return this.x;
-		}
-		public int getY() {
-			return this.y;
-		}
-	}
-
 	public int setup(String arg, ImagePlus imp) {
 		return DOES_ALL+SUPPORTS_MASKING;
 	}
@@ -37,35 +20,40 @@ public class Harris_Eckendetektor implements PlugInFilter {
 		GenericDialog gd = new GenericDialog("Optionen");
 		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
 		gd.setSize(200, 100);
-		gd.addNumericField("Kappa: ", 1.0, 2);
-		gd.addNumericField("Tau: ", 4.0, 2);
+		gd.addNumericField("Empfindlichkeit: ", 0.5, 2);
+		gd.addNumericField("Schwellwert: ", 4.0, 2);
+		gd.addNumericField("Radius: ", 1, 0);
+		gd.addCheckbox("Zeige Gradientenbilder", true);
 		gd.setLocation((int)screensize.getWidth()/2, (int)screensize.getHeight()/2);
 		gd.showDialog();
 		
 		if (gd.wasCanceled()) return;
 		
 		/* Empfindlichkeit der Eckendetektion */
-		// double kappa = gd.getNextNumber() / 100.0;
-		double kappa = gd.getNextNumber();
+		double i = gd.getNextNumber();
 		
 		/* Schwellwert */
-		// double tau = gd.getNextNumber() * 100000000.0;
-		double tau = gd.getNextNumber();
+		double t = gd.getNextNumber();
+	
+		int RAD = (int)gd.getNextNumber();
+		
+		/* Flag für Gradientenbilder */
+		boolean show_grad = gd.getNextBoolean();
 		
 		/* Schleifenzähler */
-		int x, y, i, j;
+		int x, y;
 
 		/* Bilddimensionen */
 		int w = ip.getWidth();
 		int h = ip.getHeight();
 		
-		double s_x, s_y;
+		double s_x, s_y, s_e;
 		
 		/* "Strukturmatrix" M
 		 *		/ A	C \
 		 * 		\ C	B /
 		 */
-		double A, B, C, c;
+		double A, B, C;
 		
 		/* hier landen dann die Eck-Kandidaten */
 		ArrayList<Point> edges = new ArrayList<Point>();
@@ -73,43 +61,83 @@ public class Harris_Eckendetektor implements PlugInFilter {
 		/* lege Graustufenkopie an */
 		ImageProcessor ip_gs = ip.convertToFloat();
 		
+		/* Gradientenbilder */
+		ByteProcessor ip_gradx = new ByteProcessor(w, h);
+		ByteProcessor ip_grady = new ByteProcessor(w, h);
+		ByteProcessor ip_edges = new ByteProcessor(w, h);
+		
 		/* Algorithmus 16.1 [Nischwitz] */
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				/* bestimme partielle Ableitungen; negative Werte werden im nächsten Schritt positiv */
-				s_x = (double)((ip_gs.getPixelValue(x+1, y) - ip_gs.getPixelValue(x, y)) +
-							   (ip_gs.getPixelValue(x-1, y) - ip_gs.getPixelValue(x, y))) / 255.0;
-				s_y = (double)((ip_gs.getPixelValue(x, y+1) - ip_gs.getPixelValue(x, y)) +
-							   (ip_gs.getPixelValue(x, y-1) - ip_gs.getPixelValue(x, y))) / 255.0;
+		for (y = RAD; y < h - RAD; y++) {
+			for (x = RAD; x < w - RAD; x++) {
+				// /* bestimme partielle Ableitungen; negative Werte werden im nächsten Schritt positiv */
+				// s_x = (double)((ip_gs.getPixelValue(x+1, y) - ip_gs.getPixelValue(x, y)) +
+							   // (ip_gs.getPixelValue(x-1, y) - ip_gs.getPixelValue(x, y))) / 255.0;
+				// s_y = (double)((ip_gs.getPixelValue(x, y+1) - ip_gs.getPixelValue(x, y)) +
+							   // (ip_gs.getPixelValue(x, y-1) - ip_gs.getPixelValue(x, y))) / 255.0;
+				
+				// /* bestimme M */
+				// A = s_x * s_x;
+				// B = s_y * s_y;
+				// C = s_x * s_y;
+				
+				// c = Math.abs((A * B) - (C * C) - i * Math.pow(A + B, 2.0));
+			
+				s_x = Math.abs(ip_gs.getPixelValue(x, y) - ip_gs.getPixelValue(x-RAD, y))
+					+ Math.abs(ip_gs.getPixelValue(x, y) - ip_gs.getPixelValue(x+RAD, y));
+				
+				// s_x /= 2;
+				
+				s_y = Math.abs(ip_gs.getPixelValue(x, y) - ip_gs.getPixelValue(x, y+RAD))
+					+ Math.abs(ip_gs.getPixelValue(x, y) - ip_gs.getPixelValue(x, y-RAD));
+				
+				// s_y /= 2;
+				
+				ip_gradx.putPixelValue(x, y, s_x);
+				ip_grady.putPixelValue(x, y, s_y);
 				
 				/* bestimme M */
 				A = s_x * s_x;
 				B = s_y * s_y;
 				C = s_x * s_y;
 				
-				c = Math.abs((A * B) - (C * C) - kappa * Math.pow(A + B, 2.0));
+				/* C(x,y) */
+				s_e = (A * B) - (C * C) - i * Math.pow(A + B, 2.0);
+				IJ.log("A="+A+", B="+B+", C="+C+", s_e="+s_e);
 				
-				/* falls mögliche Ecke (C(x, y) >= tau), speichere Koordinaten */
-				if (c >= tau) {
-					edges.add(new Point(x, y));
-				}
+				ip_edges.putPixelValue(x, y, s_e);
+				
+				// /* falls mögliche Ecke (C(x, y) >= tau), speichere Koordinaten */
+				// if (s_e >= t) {
+					// edges.add(new Point(x, y));
+				// }
 			}
 		}
 		
-		// TODO kacheln und lokale Schwellwerte ermitteln
+		// TODO eventuell kacheln und lokale Schwellwerte ermitteln
 		
-		ImageProcessor ip_out = ip_gs.convertToRGB();
+		// ImageProcessor ip_out = ip.duplicate();
 		
-		/* Ecken einkreisen */
-		ip_out.setColor(0xff00ff);
-		ip_out.setLineWidth(1);
+		// /* Ecken einkreisen */
+		// ip_out.setColor(0xff00ff);
+		// ip_out.setLineWidth(1);
 		
 		for (Point p: edges) {
-			ip_out.drawOval(p.getX()-3, p.getY()-3, 6, 6);
+			// ip_out.drawOval((int)p.getX()-3, (int)p.getY()-3, 6, 6);
+			// if (ip_edges.getPixelValue((int)p.getX(), (int)p.getY()) != 0.0) {
+				// IJ.log(""+ ip_edges.getPixelValue((int)p.getX(), (int)p.getY()));
+			// }
 		}
+
+        // IJ.log("Harris Eckendetektor: i="+i+", t="+t+", #edges="+edges.size());
 		
-		/* Ausgabe anzeigen */
-		new ImagePlus("Hier sind die Ecken!", ip_out).show();
+		// /* Ausgabe anzeigen */
+		// new ImagePlus("Hier sind die Ecken!", ip_out).show();
+		
+		if (show_grad) {
+			// new ImagePlus("Gradient X", ip_gradx).show();
+			// new ImagePlus("Gradient Y", ip_grady).show();
+			// new ImagePlus("Eckkandidaten", ip_edges).show();
+		}
 	}
 
 }
